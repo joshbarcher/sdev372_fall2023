89b4ad6a2582939d44c44d4c399573e045dfc4a8
Josh Archer <jarcher@greenriver.edu>
2023-10-24 13:50:37 -0700

    Updated our example to add to the DOM.

--- jokes-api/src/main/resources/static/js/script.js
+++ jokes-api/src/main/resources/static/js/script.js
@@ -15,8 +15,11 @@ async function fetchJokes()
     //use the await, async keywords to make your
     //code appear more synchronous
     let response = await fetch(uri, config);
+
+    //convert the body of the response to JSON format
+    //using JSON.parse()
     let json = await response.json();
-    console.log(json);
+    addCards(json);
 
 /*    fetch(uri, config)
         .then(function(response) {
@@ -26,6 +29,22 @@ async function fetchJokes()
         .then(function(json) {
             console.log(json);
         });*/
+}
 
-    console.log("fetchJokes() ended");
+function addCards(jokesArray)
+{
+    //loop over my array
+    for (let i = 0; i < jokesArray.length + 1; i++)
+    {
+        let joke = jokesArray[i];
+
+        //assemble the HTML for a joke using a string-template literal
+        let html = `<div class="card">
+                <h1>Joke #<span id="joke-id">${joke.id}</span></h1>
+                <p id="joke-body">${joke.jokeText}</p>
+            </div>`;
+
+        let section = document.querySelector("#jokes-list");
+        section.innerHTML = html;
+    }
 }
